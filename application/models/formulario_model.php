fb67c94090eb6a63af3040ba2dd51a9055fa4a87
Adrian <dbarra@agetic.gob.bo>
2019-07-29 12:18:19 -0400

    cambios

--- application/models/formulario_model.php
+++ application/models/formulario_model.php
@@ -3,11 +3,11 @@ if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 class Formulario_model extends CI_Model {
   public function __construct() {
     parent::__construct();
-    $this->load->database();
+    // $this->load->database();
   }
 
   public function selFormulario() {
-    $query = $this->db->query("SELECT * from form");
+    $query = $this->db->query("SELECT * FROM form");
     return $query->result();
   }
 }
