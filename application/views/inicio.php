f63427ece07a69286f669f4277cf57f1af05c7d9
Adrian <dbarra@agetic.gob.bo>
2019-08-13 14:54:58 -0400

    cambios

--- application/views/inicio.php
+++ application/views/inicio.php
@@ -5,7 +5,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Formularios</title>
-  <link rel="stylesheet" href="<?php echo base_url('public/css/vendor.min.css')?>">
+  <link rel="stylesheet" href="<?php echo base_url('public/css/leaflet.css')?>">
+  <link rel="stylesheet" href="<?php echo base_url('public/css/formio.full.min.css')?>">
+  <link rel="stylesheet" href="<?php echo base_url('public/css/bootstrap.min.css')?>">
   <style type="text/css">
     #map {
       height: 400px;
@@ -14,19 +16,11 @@
   </style>
 </head>
 <body>
-  <div id="template"></div>
-  <div class="rendered-form"></div>
-  <button id="edit-form">Edit Form</button>
-  <script type="text/javascript" src="<?php echo base_url('public/js/jquery-1.10.2.js')?>"></script>
-  <script type="text/javascript" src="<?php echo base_url('public/js/jquery-ui.min.js')?>"></script>
-  <script type="text/javascript" src="<?php echo base_url('public/js/form-builder.min.js')?>"></script>
-  <script type="text/javascript" src="<?php echo base_url('public/js/form-render.min.js')?>"></script>
-  <link rel="stylesheet" href="https://unpkg.com/leaflet@1.5.1/dist/leaflet.css" integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ==" crossorigin=""/>
-   <script src="https://unpkg.com/leaflet@1.5.1/dist/leaflet.js" integrity="sha512-GffPMF3RvMeYyc1LWMHtK8EbPv0iNZ8/oTtHPx9/cc2ILxQ+u905qIwdpULaqDkyBKgOaB57QTMg7ztg8Jm2Og==" crossorigin=""></script>
+  <div id="builder"></div>
+  <div id="formio"></div>
+  <script type="text/javascript" src="<?php echo base_url('public/js/formio.full.min.js')?>"></script>
+  <script type="text/javascript" src="<?php echo base_url('public/js/leaflet.js')?>"></script>
   <script>
-    function toggleEdit(editing) {
-      document.body.classList.toggle('rendered-form', !editing);
-    }
     function initMap (init=false) {
       if (init) {
         const map = L.map('map').setView([-16.5106572, -68.1288186], 13);
@@ -37,44 +31,44 @@
         }).addTo(map);
       }
     }
-    let fields = [{
-      label: 'Map',
-      attrs: {
-        type: 'map',
-        subType: 'component-map'
-      },
-      icon: '🌟'
-    }];
-    let templates = {
-      map: (fieldData) => {
-        return {
-          field: '<span id="' + fieldData.name + '">',
-          onRender: function() {
-            $(document.getElementById(fieldData.name)).append('<div id="map"></div>');
-            initMap(true);
+    window.onload = () => {
+      Formio.builder(document.getElementById('builder'), {}, {
+        language: 'sp',
+        i18n: {
+          sp: {
+            'Title': 'Titulo',
+            'Submit': 'Enviar',
+            'Please correct all errors before submitting.': 'Por favor, corrija todos los errores antes de enviar.',
+            'My custom error message' : 'Mi mensaje de error personalizado',
+            required : '{{field}} es requerido.',
+            invalid_email: '{{field}} debe ser un correo electrónico válido.',
+            error : 'Por favor, corrija los siguientes errores antes de enviar.',
           }
-        };
-      }
-    };
-
-    $(document).ready(() => {
-      var templateForm = document.getElementById('template');
-      var options = {
-        scrollToFieldOnAdd: false,
-        showActionButtons: true,
-        onSave: function(evt, formData) {
-          toggleEdit(false);
-          $('.rendered-form').formRender({formData});
         },
-        fields,
-        templates
-      };
-      $(templateForm).formBuilder(options);
+        builder: {
+          basic: false,
+          advanced: false,
+          layout: false,
+          data: false,
+          customBasic: {
+            title: 'Components',
+            default: true,
+            weight: 0,
+            components: {
+              textfield: true,
+              textarea: true,
+              email: true,
+              phoneNumber: true
+            }
+          }
+        }
+      }).then(function(builder) {
+      builder.on('saveComponent', function() {
+        console.log(builder.schema);
+      });
     });
-
-      document.getElementById('edit-form').onclick = function() {
-        toggleEdit(true);
-      };
+      // Formio.createForm(document.getElementById('formio'), 'https://examples.form.io/example');
+    };
   	</script>
 </body>
 </html>
