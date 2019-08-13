52cc4565a75dd434221eb9dd786a354e640e4f57
Adrian <dbarra@agetic.gob.bo>
2019-08-13 16:37:50 -0400

    cambios

--- application/views/formulario/index.php
+++
@@ -1,3 +0,0 @@
-<!-- Aqui estara el crud del generador de formularios -->
-<h1>Hola</h1>
-<?php var_dump($selFormulario) ?>
--- application/views/inicio.php
+++ application/views/inicio.php
@@ -21,7 +21,41 @@
   <script type="text/javascript" src="<?php echo base_url('public/js/formio.full.min.js')?>"></script>
   <script type="text/javascript" src="<?php echo base_url('public/js/leaflet.js')?>"></script>
   <script>
-    function initMap (init=false) {
+    const espaniol = {
+      Submit: 'Enviar',
+      Language: 'Idioma',
+      Translations: 'Traducciones',
+      'First Name': 'Tu nombre',
+      'Last Name': 'Apellido',
+      'Text Field': 'Campo de texto',
+      'Text Area': 'Parrafo',
+      Email: 'Correo electrónico',
+      'Phone Number': 'Número telefónico',
+      Save: 'Guardar',
+      Remove: 'Borrar',
+      Cancel: 'Cancelar',
+      Preview: 'Vista previa',
+      Help: 'Ayuda',
+      Display: 'Vista',
+      Data: 'Datos',
+      Validation: 'Validaciones',
+      API: 'API',
+      Conditional: 'Condicionales',
+      Logic: 'Logica',
+      Layout: 'Marco',
+      minLength: 'Faltan caracteres para {{field}}',
+      maxLength: '{{field}} excede la cantidad permitida de caracteres',
+      'Enter your email address': 'Ingrese su dirección de correo electrónico',
+      'Enter your first name': 'Ponga su primer nombre',
+      'Drag and Drop a form component': 'Arrastra y suelta en esta sección un campo de formulario',
+      'Enter your last name': 'Ingresa tu apellido',
+      'Valid Email Address': 'dirección de email válida',
+      'Please correct all errors before submitting.': 'Por favor, corrija todos los errores antes de enviar.',
+      required: '{{field}} es requerido.',
+      invalid_email: '{{field}} debe ser un correo electrónico válido.',
+      error: 'Por favor, corrija los siguientes errores antes de enviar.'
+    };
+    const initMap = (init=false) => {
       if (init) {
         const map = L.map('map').setView([-16.5106572, -68.1288186], 13);
         L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
@@ -33,17 +67,10 @@
     }
     window.onload = () => {
       Formio.builder(document.getElementById('builder'), {}, {
-        language: 'sp',
+        reandOnly: false,
+        language: 'es',
         i18n: {
-          sp: {
-            'Title': 'Titulo',
-            'Submit': 'Enviar',
-            'Please correct all errors before submitting.': 'Por favor, corrija todos los errores antes de enviar.',
-            'My custom error message' : 'Mi mensaje de error personalizado',
-            required : '{{field}} es requerido.',
-            invalid_email: '{{field}} debe ser un correo electrónico válido.',
-            error : 'Por favor, corrija los siguientes errores antes de enviar.',
-          }
+          'es': espaniol
         },
         builder: {
           basic: false,
@@ -51,7 +78,7 @@
           layout: false,
           data: false,
           customBasic: {
-            title: 'Components',
+            title: 'Campos de formulario',
             default: true,
             weight: 0,
             components: {
@@ -61,12 +88,58 @@
               phoneNumber: true
             }
           }
+        },
+        editForm: {
+          textfield: [
+            {
+              key: 'api',
+              ignore: true
+            },
+            {
+              key: 'data',
+              ignore: true
+            },
+            {
+              key: 'conditional',
+              ignore: true
+            },
+            {
+              key: 'logic',
+              ignore: true
+            },
+            {
+              key: 'layout',
+              ignore: true
+            }
+          ],
+          textarea: [
+            {
+              key: 'api',
+              ignore: true
+            },
+            {
+              key: 'data',
+              ignore: true
+            },
+            {
+              key: 'conditional',
+              ignore: true
+            },
+            {
+              key: 'logic',
+              ignore: true
+            },
+            {
+              key: 'layout',
+              ignore: true
+            }
+          ],
         }
       }).then(function(builder) {
-      builder.on('saveComponent', function() {
-        console.log(builder.schema);
+        builder.on('saveComponent', function() {
+          console.log(builder.schema);
+        });
       });
-    });
       // Formio.createForm(document.getElementById('formio'), 'https://examples.form.io/example');
     };
   	</script>
